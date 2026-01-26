46b780af1a45ff4815bc03a48fab5bbe04b1e90e
Usman Baig <mubaig@pm.me>
2026-01-26 21:52:23 +0100

    chore: bump version to 0.0.21 and add new icons BookOpenIcon and ExternalLinkIcon to index.ts

---
+++ src/icons/generated/BookOpenIcon.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgBookOpenIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="square"
+    strokeWidth={2}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
+    <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
+  </svg>
+);
+export default SvgBookOpenIcon;
