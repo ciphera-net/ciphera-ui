a5398fd51078da1abd6508dfb888fa252d2dc72c
Usman Baig <mubaig@pm.me>
2026-01-27 22:39:03 +0100

    chore: bump version to 0.0.25 and update systemTheme logic in ThemeProviders for better handling of mounted state

--- src/components/ThemeProviders.tsx
+++ src/components/ThemeProviders.tsx
@@ -88,7 +88,7 @@ export function ThemeProvider({
       setThemeState(newTheme)
     },
     resolvedTheme,
-    systemTheme: mounted && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
+    systemTheme: mounted ? (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light') : undefined,
     themes: ['light', 'dark', 'system']
   }
 
